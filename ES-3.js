6c487ad21784f15d1764931e4936620146d8fb3b
SM-Rakibul-Hasan-Rakib <devrakibulhasanrakib@gmail.com>
2024-02-22 16:36:35 +0600

    kl

---
+++ ES-3.js
@@ -0,0 +1,10 @@
+// const add2 = function(a, b){
+//   return a + b;
+// }
+
+// arrow funcation
+const add = (a, b) => a + b
+const add1 = (num1, num2, num3, num4) => num1 +num2+ num+ num4;
+// const sum = add(5, 93);
+const sum2 = add1(2, 4, 5 ,6);
+console.log(sum2);
