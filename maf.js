e202628afa185cfe7727d3d65ec7c6312327f969
SM-Rakibul-Hasan-Rakib <devrakibulhasanrakib@gmail.com>
2024-02-22 22:03:53 +0600

    f

---
+++ maf.js
@@ -0,0 +1,23 @@
+// map ==> loops though each element of the array 
+
+const numbers = [4, 5, 2, 8, 10];
+
+function doubleIt(num) {
+  console.log('num now', num);
+  return num * 2;
+}
+
+const result  = numbers.map
+
+
+
+
+
+// const doubled = [];
+// for(const num of numbers){
+//   const double = num * 2;
+//   doubled.push(double) 
+// }
+
+
+
