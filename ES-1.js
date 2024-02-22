164fd7980ba9a96020b4201771a8e345a84fadea
SM-Rakibul-Hasan-Rakib <devrakibulhasanrakib@gmail.com>
2024-02-22 15:06:16 +0600

    gh

--- ES-1.js
+++ ES-1.js
@@ -7,3 +7,21 @@ const money = 26;
 const rich = money + 500 ;
 console.log(rich);
 
+let count = 10;
+count = count + 10 ;
+console.log(count); 
+
+const number = [23, 4 , 24 ,13 ,45 ];
+number[1] = 55;
+number.push(8,9,56,45);
+console.log(number);
+
+// object 
+
+const student = {
+  name: 'moyna pakhi',
+  class: 12,
+  age:90,
+}
+student,name = 'moyna khonti',
+console.log(student);
