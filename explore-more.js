b382ffe6d99f9f80eaec58ed0fe689a4726cd4c0
SM-Rakibul-Hasan-Rakib <devrakibulhasanrakib@gmail.com>
2024-02-22 21:06:07 +0600

    d

---
+++ explore-more.js
@@ -0,0 +1,15 @@
+const a = 56;
+const numbers = [56, 7, 8];
+const person = {
+  name: "sakib khan",
+};
+const message = `Hi, ${person.name} has a: $ access to ${numbers[2]}`;
+
+if (true) {
+}
+const square = (x) => x * x;
+const sum = (a, b) => a + b;
+
+const {age, z, ...others} = {x:2, y:5, z:3, name:'ovijeet'};
+
+const [first, second, ...remaining] = ['ram', 'sam', 'jodu', 'modhu']
