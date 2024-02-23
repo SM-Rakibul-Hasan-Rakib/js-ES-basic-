dba2ff44016014a924e6cb19d99ef585bdaa6bb6
SM-Rakibul-Hasan-Rakib <devrakibulhasanrakib@gmail.com>
2024-02-23 17:54:17 +0600

    task-5 complete

--- parties-task/task-4.js
+++ parties-task/task-4.js
@@ -1,9 +1,18 @@
 // odd numbers 
-const oddArray = [1, 3, 5, 7, 9];
-const evenArray = [];
+// const oddArray = [1, 3, 5, 7, 9];
+// const evenArray = [];
 
-for(let i = 0; oddArray.length; i++) {
-  evenArray.push(oddArray[i] * 2);
+// for(let i = 0; oddArray.length; i++) {
+//   evenArray.push(oddArray[i] * 2);
+// }
+
+// console.log("Even Array using for loop:", evenArray);
+
+const oddArray = [1, 3, 5, 7, 9];
+const evenArrayUsingForLoop = [];
+
+for (let i = 0; i < oddArray.length; i++) {
+  evenArrayUsingForLoop.push(oddArray[i] * 2);
 }
 
-console.log("Even Array using for loop:", evenArray);
+console.log("Even Array using for loop:", evenArrayUsingForLoop);
